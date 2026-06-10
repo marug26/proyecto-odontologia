707ed0663e9ee2fe44618abb3b3d9367fdcba818
marcela <marcelaruiz1996@gmail.com>
2026-06-10 16:52:52 -0500

    initial commit backend

---
+++ src/main/java/com/odontologia/OdontologiaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.odontologia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdontologiaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdontologiaDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/odontologia/OdontologiaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odontologia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdontologiaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
