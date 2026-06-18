69fd20960ab28d53f4a9fc8518783e2eb6a2137c
marcela <marcelaruiz1996@gmail.com>
2026-06-18 14:04:13 -0500

    fallas oauth (no solucionado)

--- src/main/java/com/odontologia/OdontologiaDemoApplication.java
+++ src/main/java/com/odontologia/OdontologiaDemoApplication.java
@@ -1,18 +1,28 @@
 package com.odontologia;
 
-import com.odontologia.config.AuditAwareImpl;
 import java.util.UUID;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import com.odontologia.config.AuditAwareImpl;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class OdontologiaDemoApplication {
 
   public static void main(String[] args) {
+
+    Dotenv dotenv = Dotenv.load();
+    
+    System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
+    System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
+
     SpringApplication.run(OdontologiaDemoApplication.class, args);
   }
 
